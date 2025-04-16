b968d0a69d20e0856e4ba0699b073d307f05f910
Sathwik Aileneni <skaneni@gmail.com>
2025-04-16 11:26:43 +0530

    singleton class decorator added

--- decorators/common.py
+++ decorators/common.py
@@ -49,3 +49,14 @@ def validate_query_params(func):
         return wrapper
 
     return decorator
+
+
+def singleton_class(cls):
+    instances = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper
